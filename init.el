7eac575f91deb493d5bd926baa9e7666156f51cc
Zhen NI <48048142+Zhen-Ni@users.noreply.github.com>
2022-05-29 20:59:08 +0800

    Add files via upload

---
+++ init.el
@@ -0,0 +1,21 @@
+;;; The settings are in "configuration.org" for better organiztion.
+
+
+;; The settings are organized in the org file.
+(org-babel-load-file "~/.emacs.d/configuration.org")
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(multiple-cursors marginalia mwim ace-window page-break-lines rainbow-delimiters python-mode htmlize flycheck-pyflakes flycheck flycheck-rust rust-mode counsel elpy rainbow-mode markdown-mode yasnippet apdl-mode company)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
