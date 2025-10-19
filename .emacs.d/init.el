05f73d3625fcb5d30a3b5e4d85f624df9ca29f87
Zhen NI <z.ni@hotmail.com>
2025-10-19 18:13:08 +0800

    Switch to lsp-mode for python

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,5 @@
 ;;; The settings are in "configuration.org" for better organiztion.
 
-
 ;; The settings are organized in the org file.
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
@@ -10,8 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(multiple-cursors marginalia mwim ace-window page-break-lines rainbow-delimiters python-mode htmlize flycheck-pyflakes flycheck flycheck-rust rust-mode counsel elpy rainbow-mode markdown-mode yasnippet apdl-mode company)))
+ '(package-selected-packages nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
